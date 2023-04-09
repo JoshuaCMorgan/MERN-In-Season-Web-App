dcaa37f10de0f8a174244bde34b0831fb2595682
Joshua Morgan <jmorg0605@gmail.com>
2023-04-08 20:31:05 -0400

    ISSUE-3--Add-Navigation

---
+++ src/Pages/About/index.js
@@ -0,0 +1,5 @@
+function About() {
+  return <div data-testid="about-wrapper">ABOUT ME</div>;
+}
+
+export default About;
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <div data-testid="home-wrapper">HOME</div>;
+}
+
+export default Home;
