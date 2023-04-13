23831db594ade4061a296c3957a7a35fad39ac92
Joshua Morgan <jmorg0605@gmail.com>
2023-04-13 18:24:04 -0400

    refresh start on material...early start on navbar

---
+++ src/data.jsx
@@ -0,0 +1,41 @@
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "/availability-calendar",
+    text: "availability calendar",
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: "https://www.twitter.com",
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://www.twitter.com",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: "https://www.twitter.com",
+    icon: <FaBehance />,
+  },
+];
