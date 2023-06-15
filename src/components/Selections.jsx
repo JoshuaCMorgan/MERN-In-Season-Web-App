06fde80655a59d41fd16dbda62c7fa922b69ea99
Joshua Morgan <jmorg0605@gmail.com>
2023-06-14 20:35:06 -0400

    updated appearance of item container items

--- src/components/Selections.jsx
+++ src/components/Selections.jsx
@@ -1,17 +1,17 @@
 const Selections = ({ produce }) => {
   return (
-    <section className='produceQuery'>
+    <section className="produceQuery">
       <h1>Your Selection: </h1>
       <ul>
         {produce.map((item, idx) => {
           return (
-            <li key={idx} className='item-container'>
+            <li key={idx} className="item-container">
               <h4>{item.name}</h4>
               <figure>
-                <img src='' alt='' />
+                <img src="" alt="" />
               </figure>
               <p>{item.desc}</p>
-              <button>Add Item to Shopping List</button>
+              <button>Add to Shopping List</button>
             </li>
           );
         })}
