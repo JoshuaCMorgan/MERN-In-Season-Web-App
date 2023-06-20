3c78053d2e0890cf4242b8311493580def3b580a
Joshua Morgan <jmorg0605@gmail.com>
2023-06-20 18:52:07 -0400

    initial register page update npm

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,52 @@
+/** @jsxImportSource @emotion/react */
+import { jsx } from "@emotion/react";
+import styled from "@emotion/styled";
+import { useState, useEffect } from "react";
+import { Logo } from "../components";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
+function Register() {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <div className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </div>
+  );
+}
