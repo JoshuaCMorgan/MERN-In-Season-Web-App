2dd31cd1687f07fe06f4707e976b1a11553c7f94
Joshua Morgan <jmorg0605@gmail.com>
2023-06-20 20:17:11 -0400

    completed register page
    added form row component, alert component and toggle member

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,23 @@
 /** @jsxImportSource @emotion/react */
-import { jsx } from "@emotion/react";
+import { jsx, css } from "@emotion/react";
 import styled from "@emotion/styled";
 import { useState, useEffect } from "react";
-import { Logo } from "../components";
+import { Logo, FormRow, Alert } from "../components";
 
 const initialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
+  showAlert: true,
 };
 
 export function Register() {
   const [values, setValues] = useState(initialState);
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
 
   const handleChange = (e) => {
     console.log(e.target);
@@ -22,31 +27,92 @@ export function Register() {
     e.preventDefault();
     console.log(e.target);
   };
+
   return (
-    <div className="full-page">
-      <form className="form" onSubmit={onSubmit}>
-        <Logo />
-        <h3>Login</h3>
-
-        {/* name field */}
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-
-          <input
-            type="text"
+    <FormWrapper className="full-page">
+      <form css={formCss} className="form" onSubmit={onSubmit}>
+        <Logo cssOverrides={logoCss} />
+        <h3 css={h3Css}>{values.isMember ? "Login" : "Register"}</h3>
+        {values.showAlert && <Alert />}
+        {values.isMember && (
+          <FormRow
+            type="name"
             value={values.name}
             name="name"
-            onChange={handleChange}
-            className="form-input"
+            handleChange={handleChange}
           />
-        </div>
+        )}
 
-        <button type="submit" className="btn btn-block">
+        <FormRow
+          type="email"
+          value={values.email}
+          name="email"
+          handleChange={handleChange}
+        />
+        <FormRow
+          type="password"
+          value={values.password}
+          name="password"
+          handleChange={handleChange}
+        />
+
+        <button css={btnCss} type="submit" className="btn btn-block">
           submit
         </button>
+        <p css={pCss}>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button
+            css={memberBtnCss}
+            type="button"
+            onClick={toggleMember}
+            className="member-btn"
+          >
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
       </form>
-    </div>
+    </FormWrapper>
   );
 }
+
+const FormWrapper = styled.div({
+  display: "grid",
+  alignItems: "center",
+});
+
+const formCss = css({
+  maxWidth: "400px",
+  borderTop: "5px solid var(--primary-500)",
+  lineHeight: "1.75",
+});
+
+const logoCss = css({
+  display: "block",
+  margin: "0 auto",
+  marginBottom: "1.38rem",
+});
+
+const pCss = css({
+  margin: "0",
+  marginTop: "1rem",
+  textAlign: "center",
+});
+
+const btnCss = css({
+  marginTop: "1rem",
+});
+
+const memberBtnCss = css({
+  background: "transparent",
+  border: "transparent",
+  color: "var(--primary-500)",
+  cursor: "pointer",
+  letterSpacing: "var(--letterSpacing)",
+  padding: "1px 6px",
+});
+
+const h3Css = css({
+  textAlign: "center",
+
+  marginBottom: `1.38rem`,
+});
