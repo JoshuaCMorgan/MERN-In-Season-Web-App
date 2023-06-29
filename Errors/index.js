0741565c52ac0d3a63f124fba444193b09cccb35
Joshua Morgan <jmorg0605@gmail.com>
2023-06-28 21:05:40 -0400

    created CustomAPIError class and errors

---
+++ Errors/index.js
@@ -0,0 +1,5 @@
+import CustomAPIError from "./custom-api.js";
+import BadRequestError from "./bad-request.js";
+import NotFoundError from "./not-found.js";
+
+export { CustomAPIError, BadRequestError, NotFoundError };
--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,18 @@ import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
