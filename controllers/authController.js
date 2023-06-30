89c6df9b63d48dd27c3bb0719785541b6438a3cd
Joshua Morgan <jmorg0605@gmail.com>
2023-06-29 22:05:28 -0400

    setup jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,8 @@ const register = async (req, res) => {
   }
 
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user, token });
 };
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -36,10 +38,15 @@ const UserSchema = new mongoose.Schema({
     default: "my city",
   },
 });
+
 // Hook gets called before saving document
 UserSchema.pre("save", async function () {
   console.log(this.password);
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, "jwtSecret", { expiresIn: "1d" });
+};
 export default mongoose.model("User", UserSchema);
