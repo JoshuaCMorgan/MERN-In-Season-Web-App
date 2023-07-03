ece5c95336c323355249aabf04e1fcd262566888
Joshua Morgan <jmorg0605@gmail.com>
2023-07-03 19:33:07 -0400

    message to user

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,10 +2,11 @@ import heroImg from "../assets/images/fruitsveggies.jpg";
 
 const Hero = () => {
   return (
-    <section className='hero' id='home'>
-      <div className='hero-banner'>
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>Working on connecting to backend. Check back soon!</h1>
         <h1>What's In Season: </h1>
-        <div className='scroll-container'>
+        <div className="scroll-container">
           <p>...in your state</p>
           <p>...right now</p>
           <p>...at your market</p>
