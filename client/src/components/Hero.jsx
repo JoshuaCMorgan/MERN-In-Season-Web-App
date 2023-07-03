478fca689dd9a38d324a7032021f843692c5d32e
Joshua Morgan <jmorg0605@gmail.com>
2023-07-03 19:58:23 -0400

    restored FE for time being

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,7 +4,6 @@ const Hero = () => {
   return (
     <section className="hero" id="home">
       <div className="hero-banner">
-        <h1>Working on connecting to backend. Check back soon!</h1>
         <h1>What's In Season: </h1>
         <div className="scroll-container">
           <p>...in your state</p>
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/v1": {
-        target: "http://localhost:5001",
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api/v1": {
+  //       target: "http://localhost:5001",
+  //     },
+  //   },
+  // },
 });
