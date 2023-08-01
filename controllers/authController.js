67c64cd850ccfd62769cb0013ab460e835e5c729
Joshua Morgan <jmorg0605@gmail.com>
2023-08-01 19:53:19 -0400

    create token and http cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,9 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 import bcrypt from "bcryptjs";
 import { hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
+import { comparePassword } from "../utils/passwordUtils.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
 export const register = async (req, res) => {
   // If document count of users is zero (no users) then first user is admin role
@@ -17,5 +20,21 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login");
+  const user = await User.findOne({ email: req.body.email });
+  // check if user exists and password correct
+  const isValidUser =
+    user && (await await comparePassword(req.body.password, user.password));
+
+  if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
+
+  const token = createJWT({ userId: user._id, role: user.role });
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,12 @@ import express from "express";
 const router = express.Router();
 
 import { register, login } from "../controllers/authController.js";
-import { validateRegisterInput } from "../middleware/validationMiddleware.js";
-validateRegisterInput;
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from "../middleware/validationMiddleware.js";
 
 router.route("/register").post(validateRegisterInput, register);
-router.route("/login").post(login);
+router.route("/login").post(validateLoginInput, login);
 
 export default router;
