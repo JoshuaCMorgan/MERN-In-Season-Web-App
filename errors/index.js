42ff2b67d14e6731638f37790d01aa1e9116ecc4
Joshua Morgan <jmorg0605@gmail.com>
2023-08-01 11:00:52 -0400

    combined error to single file

--- errors/index.js
+++
@@ -1,6 +0,0 @@
-import CustomAPIError from "./custom-api.js";
-import BadRequestError from "./bad-request.js";
-import NotFoundError from "./not-found.js";
-import UnAuthenticatedError from "./unAuthenticated.js";
-
-export { CustomAPIError, BadRequestError, NotFoundError, UnAuthenticatedError };
