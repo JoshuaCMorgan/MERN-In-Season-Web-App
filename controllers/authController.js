a537efeded2b9774efb63b5a9aa3f40380258578
Joshua Morgan <jmorg0605@gmail.com>
2023-08-01 18:07:48 -0400

    create admin user

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,10 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 
 export const register = async (req, res) => {
+  // If document count of users is zero (no users) then first user is admin role
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? "admin" : "user";
+
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
