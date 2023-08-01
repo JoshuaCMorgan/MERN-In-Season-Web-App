530cbd3f81db35a717b156839edc6c5c470649ef
Joshua Morgan <jmorg0605@gmail.com>
2023-08-01 10:14:46 -0400

    renamed files and added constants to utils folder

---
+++ utils/constants.js
@@ -0,0 +1,14 @@
+export const PRODUCE_TYPE = {
+  FRUIT: "Fruit",
+  VEGETABLE: "Vegetable",
+  HERB: "Herb",
+  LEGUME: "Legume",
+  NUT: "Nut",
+};
+
+export const PRODUCE_SORT_BY = {
+  NEWEST_FIRST: "newest",
+  OLDEST_FIRST: "oldest",
+  ASCENDING: "a-z",
+  DESCENDING: "z-a",
+};
