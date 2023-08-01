1687bffe7a378e857d98eca38dfec53b4122addf
Joshua Morgan <jmorg0605@gmail.com>
2023-08-01 18:45:45 -0400

    hash password of registered user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,19 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
   // If document count of users is zero (no users) then first user is admin role
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
 
+  // create hashed password and override user password
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
