73bcb2efb98f242521913e72c50f9295de2c2548
Joshua Morgan <jmorg0605@gmail.com>
2023-08-02 09:47:26 -0400

    logout user functionality added

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,3 +38,11 @@ export const login = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
+
+export const logout = async (req, res) => {
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { register, login } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 import {
   validateRegisterInput,
   validateLoginInput,
 } from "../middleware/validationMiddleware.js";
 
-router.route("/register").post(validateRegisterInput, register);
-router.route("/login").post(validateLoginInput, login);
+router.post("/register", validateRegisterInput, register);
+router.post("/login", validateLoginInput, login);
+router.get("/logout", logout);
 
 export default router;
