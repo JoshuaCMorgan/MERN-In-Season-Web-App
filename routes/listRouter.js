31521787fcde1aabfa498f88f39cd838796f10cd
Joshua Morgan <jmorg0605@gmail.com>
2023-08-09 19:06:11 -0400

    removed file for build issue

---
+++ routes/listRouter.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+const router = Router();
+
+import {
+  addItem,
+  deleteItem,
+  getAllItems,
+} from "../controllers/listController.js";
+
+import {
+  validateIdParam,
+  validateItemInput,
+} from "../middleware/validationMiddleware.js";
+
+router.route("/").get(getAllItems).post(validateItemInput, addItem);
+router.route("/:id").delete(validateIdParam, deleteItem);
+
+export default router;
