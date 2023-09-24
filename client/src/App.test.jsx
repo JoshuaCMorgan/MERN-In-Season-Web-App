1ec49967295fce763bdba78834a5cc32125ee34c
Joshua Morgan <jmorg0605@gmail.com>
2023-09-24 08:18:12 -0400

    updated packages and added dev vitest

---
+++ client/src/App.test.jsx
@@ -0,0 +1,23 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("something truthy and falsy", () => {
+  it("true to be true", () => {
+    expect(true).toBe(true);
+  });
+
+  it("false to be false", () => {
+    expect(false).toBe(false);
+  });
+});
+
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App />);
+
+    screen.debug();
+
+    // check if App components renders headline
+  });
+});
